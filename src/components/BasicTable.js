01c103a7146a3f75c82ee7eed8ef4ceb755de383
Suat-17 <yargicisuat@gmail.com>
2023-03-01 09:42:12 +0300

    chore: last change

--- src/components/BasicTable.js
+++ src/components/BasicTable.js
@@ -1,12 +1,12 @@
-import React, { useMemo } from 'react'
-import { useTable } from 'react-table'
-import MOCK_DATA from './MOCK_DATA.json'
-import { COLUMNS } from './columns'
-import './table.css'
+import React, { useMemo } from "react";
+import { useTable } from "react-table";
+import MOCK_DATA from "./MOCK_DATA.json";
+import { GROUPED_COLUMNS } from "./columns";
+import "./table.css";
 
 export const BasicTable = () => {
-  const columns = useMemo(() => COLUMNS, [])
-  const data = useMemo(() => MOCK_DATA, [])
+  const columns = useMemo(() => GROUPED_COLUMNS, []);
+  const data = useMemo(() => MOCK_DATA, []);
 
   const {
     getTableProps,
@@ -14,47 +14,48 @@ export const BasicTable = () => {
     headerGroups,
     footerGroups,
     rows,
-    prepareRow
+    prepareRow,
   } = useTable({
     columns,
-    data
-  })
+    data,
+  });
 
   return (
     <>
       <table {...getTableProps()}>
         <thead>
-          {headerGroups.map(headerGroup => (
+          {headerGroups.map((headerGroup) => (
             <tr {...headerGroup.getHeaderGroupProps()}>
-              {headerGroup.headers.map(column => (
-                <th {...column.getHeaderProps()}>{column.render('Header')}</th>
+              {headerGroup.headers.map((column) => (
+                <th {...column.getHeaderProps()}>{column.render("Header")}</th>
               ))}
             </tr>
           ))}
         </thead>
         <tbody {...getTableBodyProps()}>
-          {rows.map(row => {
-            prepareRow(row)
+          {rows.map((row) => {
+            prepareRow(row);
             return (
               <tr {...row.getRowProps()}>
-                {row.cells.map(cell => {
-                  return <td {...cell.getCellProps()}>{cell.render('Cell')}</td>
+                {row.cells.map((cell) => {
+                  return (
+                    <td {...cell.getCellProps()}>{cell.render("Cell")}</td>
+                  );
                 })}
               </tr>
-            )
+            );
           })}
         </tbody>
-          
         <tfoot>
-          {footerGroups.map(footerGroup => (
+          {footerGroups.map((footerGroup) => (
             <tr {...footerGroup.getFooterGroupProps()}>
-              {footerGroup.headers.map(column => (
-                <td {...column.getFooterProps()}>{column.render('Footer')}</td>
+              {footerGroup.headers.map((column) => (
+                <td {...column.getFooterProps()}>{column.render("Footer")}</td>
               ))}
             </tr>
           ))}
         </tfoot>
       </table>
     </>
-  )
-}
+  );
+};
