88a7e56824fb3aa16964cf1f9d3ea30db816339b
Suat-17 <yargicisuat@gmail.com>
2023-03-01 12:16:39 +0300

    chore: last change

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import logo from "./logo.svg";
 import "./App.css";
 import { SortingTable } from "./components/SortingTable";
+import { FilteringTable } from "./components/FilteringTable";
+import { PaginationTable } from "./components/PaginationTable";
 
 function App() {
   return (
     <div>
       <h1>Fdgdfgg</h1>
-      <SortingTable />
+      <PaginationTable />
     </div>
   );
 }
--- src/components/columns.js
+++ src/components/columns.js
@@ -1,4 +1,4 @@
-
+import { format } from 'date-fns'
 
 export const COLUMNS = [
   {
@@ -24,7 +24,9 @@ export const COLUMNS = [
     Header: 'Date of Birth',
     Footer: 'Date of Birth',
     accessor: 'date_of_birth',
-   
+    Cell: ({ value }) => {
+      return format(new Date(value), 'dd/MM/yyyy')
+    }
   },
   {
     Header: 'Country',
