6ae68a35d8c17fadec0c03639a9b76c525ebd67a
Suat-17 <yargicisuat@gmail.com>
2023-03-01 14:31:02 +0300

    chore: last change

--- src/App.js
+++ src/App.js
@@ -3,12 +3,13 @@ import "./App.css";
 import { SortingTable } from "./components/SortingTable";
 import { FilteringTable } from "./components/FilteringTable";
 import { PaginationTable } from "./components/PaginationTable";
+import { RowSelection } from "./components/RowSelection";
 
 function App() {
   return (
     <div>
       <h1>Fdgdfgg</h1>
-      <PaginationTable />
+      <RowSelection />
     </div>
   );
 }
